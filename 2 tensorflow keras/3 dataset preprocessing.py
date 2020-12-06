c7db84d700130680147cb2f6a4a81775d4dd8c49
huanhuanhiking <huanhuanhiking@gmail.com>
2020-12-06 18:12:18 -0500

    update

---
+++ 2 tensorflow keras/3 dataset preprocessing.py
@@ -0,0 +1,41 @@
+# Image data preprocessing
+
+# https://keras.io/api/preprocessing/
+
+# Use this code to generate train/validate generator from the same folder
+
+# import library
+from keras.preprocessing.image import ImageDataGenerator
+
+# set parameters
+image_size = (112,112)
+
+batch_size = 32
+
+
+class_mode = 'binary' # for multi-class classification problem, use: class_mode = 'category' 
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    fill_mode="nearest",
+    validation_split=0.2 # set validation split
+    ) 
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_directory,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode=class_mode, # for multi-class classification problem, use 'category'
+    subset='training') # set as training data
+
+validation_generator = train_datagen.flow_from_directory(
+    train_data_directory, # same directory as training data
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode=class_mode, # for multi-class classification problem, use 'category'
+    subset='validation') # set as validation data
