7f383bfaa67732980707a85a319702e9a1264168
huanhuanhiking <75589863+huanhuanhiking@users.noreply.github.com>
2020-12-07 12:51:52 -0500

    Update 3 dataset preprocessing.py

--- 2 tensorflow keras/3 dataset preprocessing.py
+++ 2 tensorflow keras/3 dataset preprocessing.py
@@ -4,18 +4,21 @@
 
 # Use this code to generate train/validate generator from the same folder
 
+# Set train_data_directory
+train_data_directory = ''
+
 # import library
 from keras.preprocessing.image import ImageDataGenerator
 
 # set parameters
-image_size = (112,112)
+image_size = (224,224)
 
-batch_size = 32
+batch_size = 128
 
-
+# binary problem class_mode
 class_mode = 'binary' # for multi-class classification problem, use: class_mode = 'category' 
 
-train_datagen = ImageDataGenerator(
+datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
     zoom_range=0.2,
@@ -26,14 +29,14 @@ train_datagen = ImageDataGenerator(
     validation_split=0.2 # set validation split
     ) 
 
-train_generator = train_datagen.flow_from_directory(
+train_generator = datagen.flow_from_directory(
     train_data_directory,
     target_size=image_size,
     batch_size=batch_size,
     class_mode=class_mode, # for multi-class classification problem, use 'category'
     subset='training') # set as training data
 
-validation_generator = train_datagen.flow_from_directory(
+validation_generator = datagen.flow_from_directory(
     train_data_directory, # same directory as training data
     target_size=image_size,
     batch_size=batch_size,
