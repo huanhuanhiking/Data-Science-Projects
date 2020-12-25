3ecb5f35397d4f761654b6c8b3ac3c48ad045641
huanhuanhiking <huanhuanhiking@gmail.com>
2020-12-25 15:01:41 -0500

    update

--- tensorflow keras/dog breeds/dogbreeds calssification.py
+++ tensorflow keras/dog breeds/dogbreeds calssification.py
@@ -3,31 +3,60 @@ import os
 os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 import keras
 
+print(keras.__version__)
+
+# set parameters
+image_size = (224,224)
+
+batch_size = 64
+
+# binary problem class_mode
+class_mode = 'categorical'
+# for binary classification problem, use: class_mode = 'binary'
+# for multi-class classification problem, use: class_mode = 'categorical'
+
+
+# build the model
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D, Conv2D, MaxPool2D
+
+# import vgg16 model
+from keras.applications.vgg16 import VGG16
+
+vgg16_conv = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+vgg16_conv.trainable = False
+
+model = Sequential()
+
+model.add(vgg16_conv)
+
+model.add(Flatten())
+model.add(Dense(1024,activation="relu"))
+model.add(Dense(1024,activation="relu"))
+
+
+model.add(Dense(47, activation="softmax"))
+
+print(model.summary())
+
 
 # Set train_data_directory
-train_data_directory = 'E:/cats and dogs/train'
+train_data_directory = 'D:/data science/Dog Breeds'
 
 # import library
 from keras.preprocessing.image import ImageDataGenerator
 
-# set parameters
-image_size = (112,112)
-
-batch_size = 128
-
-# binary problem class_mode
-class_mode = 'binary' # for multi-class classification problem, use: class_mode = 'category'
-
 datagen = ImageDataGenerator(
     rescale=1./255,
-    shear_range=0.1,
-    zoom_range=0.1,
-    rotation_range=0.1,
+    shear_range=0.2,
+    zoom_range=0.2,
+    rotation_range=0.5,
     width_shift_range=0.1,
     height_shift_range=0.1,
     horizontal_flip=True,
     fill_mode="nearest",
-    validation_split=0.33 # set validation split
+    validation_split=0.2 # set validation split
     )
 
 train_generator = datagen.flow_from_directory(
@@ -45,43 +74,14 @@ validation_generator = datagen.flow_from_directory(
     subset='validation') # set as validation data
 
 
-# building a VGG16 net
-# build a model
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D , Flatten, GlobalAveragePooling2D, Dropout, BatchNormalization
-
-VGG_16 = Sequential()
-VGG_16.add(Conv2D(input_shape=(112,112,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-VGG_16.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-VGG_16.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-VGG_16.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-VGG_16.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-VGG_16.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-VGG_16.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-VGG_16.add(Flatten())
-
-VGG_16.add(Dense(1024,activation="relu"))
-VGG_16.add(Dense(1, activation="sigmoid"))
-
-VGG_16.summary()
 
 # compile model
 from keras import optimizers
 
-VGG_16.compile(loss='binary_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
+
 
 # Callbacks
 
@@ -105,13 +105,31 @@ learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
                                             patience=1,
                                             verbose=1,
                                             factor=0.1,
-                                            min_lr=0.00001)
+                                            min_lr=0.00000001)
+
+
 nb_epochs = 1000
 
-history = VGG_16.fit_generator(train_generator,
+# from keras.models import load_model
+ 
+# load model
+# model = load_model("C:/Users/jiahu/Documents/model.h5")
+
+history = model.fit_generator(train_generator,
                                steps_per_epoch = train_generator.samples // batch_size,
                                validation_data = validation_generator,
                                validation_steps = validation_generator.samples // batch_size,
                                epochs = nb_epochs,
-                               callbacks=[checkpoint, early_stopping, learning_rate_reduction],
-                               verbose=2)
+                               callbacks=[checkpoint, early_stopping, learning_rate_reduction])
+
+
+# Unfreeze the base model
+model.trainable = True
+
+# retrain all parameter within the model
+history = model.fit_generator(train_generator,
+                               steps_per_epoch = train_generator.samples // batch_size,
+                               validation_data = validation_generator,
+                               validation_steps = validation_generator.samples // batch_size,
+                               epochs = nb_epochs,
+                               callbacks=[checkpoint, early_stopping, learning_rate_reduction])
